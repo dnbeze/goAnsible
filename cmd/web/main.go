6b58874e4d49f1feb2b25e88f9fca321750c22ec
Brett Ezell <brett@pop-os.localdomain>
2023-02-25 15:30:47 -0500

    setup some templates and working on template cache just started

---
+++ cmd/web/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"goAnsible/handlers"
+	"net/http"
+)
+
+const portNumber = ":8089"
+
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
