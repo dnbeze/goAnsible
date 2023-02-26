62639b25354d95e413bca2cfb9d37abd62ebc4c3
Brett Ezell <brett@pop-os.localdomain>
2023-02-26 02:03:11 -0500

    setup new handlers and created and setup Repository

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,14 +1,35 @@
 package handlers
 
 import (
+	"goAnsible/pkg/config"
 	"goAnsible/pkg/render"
 	"net/http"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creats a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the respository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.html")
 }
 
-func About(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about.html")
 }
