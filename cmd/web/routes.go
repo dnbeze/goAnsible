61ecefad42bca20e94c69f362e831922b876cdc2
Brett Ezell <brett@pop-os.localdomain>
2023-02-26 02:41:17 -0500

    moved routes to chi

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,15 @@ package main
 import (
 	"github.com/go-chi/chi"
 	"goAnsible/pkg/config"
+	"goAnsible/pkg/handlers"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	//mux := pat.New()
-
-	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
 	mux := chi.NewRouter()
 
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
