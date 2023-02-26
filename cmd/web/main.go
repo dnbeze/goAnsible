02222e67eb593acd8c257bc10ea2d55d26ebeba5
Brett Ezell <brett@pop-os.localdomain>
2023-02-26 03:25:34 -0500

    session inside main now reffers to global variable session

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,17 +14,18 @@ import (
 const portNumber = ":8089"
 
 var app config.AppConfig
+var session *scs.SessionManager
 
 func main() {
 
 	//change this to true when in production
 	app.InProduction = false
 
-	session := scs.New()                           //scs package handles sessions
+	session = scs.New()                            //REMEMER THIS referring to the package level var session use = not :=
 	session.Lifetime = 24 * time.Hour              // set lifetime of session to expire 24 hr
 	session.Cookie.Persist = true                  // session will persist a browser being closed - false for fast dying session
 	session.Cookie.SameSite = http.SameSiteLaxMode // TODO what does lax mode mean here probably need to see how secure this is
-	session.Cookie.Secure = app.InProduction       // TODO MUST BE TRUE IN PRODUCTION
+	session.Cookie.Secure = app.InProduction       // app.InProduction is now package level variable
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
