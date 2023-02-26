134128df7ae316a19019a0b24c7f1d5e4bcd6e65
Brett Ezell <brett@pop-os.localdomain>
2023-02-26 03:08:10 -0500

    setting up cookie for session persistence

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/alexedwards/scs/v2"
 	"goAnsible/pkg/config"
 	"goAnsible/pkg/handlers"
 	"goAnsible/pkg/render"
 	"log"
 	"net/http"
+	"time"
 )
 
 const portNumber = ":8089"
 
 func main() {
 	var app config.AppConfig
+
+	session := scs.New()              //scs package handles sessions
+	session.Lifetime = 24 * time.Hour // set lifetime of session to expire 24 hr
+	session.Cookie.Persist = true     // session will persist a browser being closed - false for fast dying sesion
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false // TODO MUST BE TRUE IN PRODUCTION
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
